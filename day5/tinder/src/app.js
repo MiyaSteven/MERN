1c3e84e0a353649c4f70ea283f989572bdc3e904
Steven <swm9220@gmail.com>
2020-07-12 13:34:44 -0700

    small fun changes

--- day5/tinder/src/app.js
+++ day5/tinder/src/app.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./css/app.css";
-
 import PlayerCard from "./components/preview";
 
 // prop-it-up & putting-it-together demo but slightly different
@@ -14,7 +13,7 @@ function App() {
       username: "Giannis Antetokounmpo",
       imgUrl:
         "https://nbatopshot.com/cdn-cgi/image/width=582/https://assets.nbatopshot.com/editions/1_from_the_top_legendary/51f4281b-a87f-47bc-ba21-eec09f1a7730/play_51f4281b-a87f-47bc-ba21-eec09f1a7730_1_from_the_top_legendary_capture_Hero_2880_2880_Black.jpg",
-      likeCount: 0,
+      pointCount: 0,
       age: 24,
       bio: "Pick 15, Round 1",
     },
@@ -23,7 +22,7 @@ function App() {
       lastName: "Curry",
       imgUrl:
         "https://nbatopshot.com/cdn-cgi/image/width=582/https://assets.nbatopshot.com/editions/1_from_the_top_legendary/f552b506-b41d-4e7c-ac5a-59427f9e96e6/play_f552b506-b41d-4e7c-ac5a-59427f9e96e6_1_from_the_top_legendary_capture_Hero_2880_2880_Black.jpg",
-      likeCount: 0,
+      pointCount: 0,
       age: 31,
       bio: "Pick 7, Round 1",
     },
@@ -35,7 +34,7 @@ function App() {
         firstName="Nice"
         lastName="Guy"
         imgUrl="https://m.media-amazon.com/images/I/91VIl9sAfRL._SS500_.jpg"
-        likeCount={0}
+        pointCount={0}
         age="24"
         bio="Finishes Last, but I deserve to be first because I'm a nice guy, I promise!"
       />
@@ -43,7 +42,7 @@ function App() {
         firstName="Yaas"
         lastName="Queen"
         imgUrl="https://www.biography.com/.image/t_share/MTE1ODA0OTcxNjIzMzUxODIx/queen-elizabeth-ii-9286165-1-402.jpg"
-        likeCount={14}
+        pointCount={14}
         age="31"
         bio="YaaAaaAaaAs queen."
       /> */}
@@ -55,7 +54,7 @@ function App() {
             firstName={data.firstName}
             lastName={data.lastName}
             imgUrl={data.imgUrl}
-            likeCount={data.likeCount}
+            pointCount={data.pointCount}
             age={data.age}
             bio={data.bio}
           />
