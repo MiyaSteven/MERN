65e1fbb6334db900af8f9de8c11ad06f15f0d65d
Steven <swm9220@gmail.com>
2020-07-12 14:01:03 -0700

    jsx base

--- day5/tinder/src/app.js
+++ day5/tinder/src/app.js
@@ -10,7 +10,6 @@ function App() {
     {
       firstName: "Giannis",
       lastName: "Antetokounmpo",
-      username: "Giannis Antetokounmpo",
       imgUrl:
         "https://nbatopshot.com/cdn-cgi/image/width=582/https://assets.nbatopshot.com/editions/1_from_the_top_legendary/51f4281b-a87f-47bc-ba21-eec09f1a7730/play_51f4281b-a87f-47bc-ba21-eec09f1a7730_1_from_the_top_legendary_capture_Hero_2880_2880_Black.jpg",
       pointCount: 0,
@@ -30,23 +29,6 @@ function App() {
 
   return (
     <div className="App">
-      {/* <PlayerCard
-        firstName="Nice"
-        lastName="Guy"
-        imgUrl="https://m.media-amazon.com/images/I/91VIl9sAfRL._SS500_.jpg"
-        pointCount={0}
-        age="24"
-        bio="Finishes Last, but I deserve to be first because I'm a nice guy, I promise!"
-      />
-      <PlayerCard
-        firstName="Yaas"
-        lastName="Queen"
-        imgUrl="https://www.biography.com/.image/t_share/MTE1ODA0OTcxNjIzMzUxODIx/queen-elizabeth-ii-9286165-1-402.jpg"
-        pointCount={14}
-        age="31"
-        bio="YaaAaaAaaAs queen."
-      /> */}
-
       {profileData.map((data, idx) => {
         return (
           <PlayerCard
