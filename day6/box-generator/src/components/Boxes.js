26f8be6ae11ba6e23906cf3e3f30d0d5aafce462
Steven <swm9220@gmail.com>
2020-07-15 17:15:13 -0700

    boxes showing up immediately but user cant add

--- day6/box-generator/src/components/Boxes.js
+++ day6/box-generator/src/components/Boxes.js
@@ -1,20 +1,46 @@
 import React, { useState } from "react";
 
 const Boxes = (props) => {
-  const [boxes, setBoxes] = useState("");
-  const [color, setColor] = useState("");
+  const [color, setBoxes] = useState("");
 
-  const boxes = {
-    color: "",
+  const boxList = [
+    {
+      color: "red",
+    },
+    {
+      color: "yellow",
+    },
+    {
+      color: "green",
+    },
+    {
+      color: "blue",
+    },
+    {
+      color: "purple",
+    },
+  ];
+
+  const createBox = (e) => {
+    e.preventDefault();
+    setBoxes({
+      color: boxList[0].color,
+    });
   };
 
-  // add a form to allow user to submit a text entry of a valid color to get returned through the jsx style and output box with background-color: {color}
   return (
-    <div id={boxes}>
-      <div
-        style={{ height: "200px", width: "200px", backgroundColor: { color } }}
-      >
-        {color} Box
+    <div>
+      <button onClick={createBox}>Create Box </button>
+      <div>
+        {boxList.map(function (box, i) {
+          let styles = {
+            backgroundColor: box.color,
+            width: "200px",
+            height: "200px",
+            display: "inline-flex",
+          };
+          return <div key={i} style={styles}></div>;
+        })}
       </div>
     </div>
   );
